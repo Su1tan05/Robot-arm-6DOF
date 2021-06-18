f9a3438121d9393b2301b1987e05baaa5135b507
Султан <sultan-avto5006@mail.ru>
2021-06-18 20:26:26 +0300

    add img

--- Inverse_Kinematics_Test.m
+++ Inverse_Kinematics_Test.m
@@ -6,7 +6,6 @@ L3=Link([ 0 0 85 0], 'standard')
 L4=Link([ 0 0 150 0], 'standard')
 robot=SerialLink([L1,L2,L3,L4]) 
 q= [0 0 0 0]
- 
 m=[1 1 1 0 0 1]
 T = [1 0 0 335
      0 1 0 0
@@ -17,10 +16,7 @@ robot.display();
 view(3);
 robot.plot([0 0 0 0]);
 robot.teach()
-
- 
 J = robot.ikine(T,'q', q,'mask',m)*180/pi
-
 Th1 = floor(J(1))+90
 Th2 = floor(J(2))
 Th3 = floor(J(3))+90
--- OZK3.m
+++
@@ -1,9 +0,0 @@
-function [Th1,Th2,Th3] = OZK3(X,Y,Z)
-l1 = 100.0
-l2 = 100.0
-l3 = 85.0+150
-% l4 = 150.0
-Th1 = abs(real(atand(Y/X)))
-Th2 = abs(real(atand(Z-l1/sqrt(X^2+Y^2))-acosd((l2^2-l3^2+X^2+Y^2+(Z-l1)^2)/(2*l2*sqrt(X^2+Y^2+(Z-l1)^2)))));
-Th3 = abs(real(-(pi - acosd((l2^2-l3^2+X^2+Y^2+(Z-l1)^2)/(2*l2*l3)))));
-end
